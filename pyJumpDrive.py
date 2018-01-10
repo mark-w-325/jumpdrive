0304ae9319628e5de850ac8e7585b8313d07338c
Mark Williams <mark.williams@bbva.com>
2018-01-10 17:32:18 -0600

    Added functionality

--- pyJumpDrive.py
+++ pyJumpDrive.py
@@ -19,6 +19,7 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.nbr_players = 1
         self.game_id = 1
+        self.round = 1
         self.game_data = self.loadGameData()
         
         self.cb_p1.clear()
@@ -49,6 +50,7 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
         self.btn_add_players.clicked.connect(self.AddPlayer)
         self.btn_remove_player.clicked.connect(self.DeletePlayer)
         self.btn_start_game.clicked.connect(self.StartGame)
+        self.btn_add_score.clicked.connect(self.AddScore)
 
     def _add_to_player_grid(self, players):
         if players == 1:
@@ -102,17 +104,28 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
             
     def StartGame(self):
         max_game_id = int(max(self.game_data['games'], key=int))
+        current_game_dict = self.game_data
+        players = self.loadPlayers()
         if max_game_id == self.game_id:
-            current_game_dict = self.game_data
-            print current_game_dict
-            players = self.getPlayers()
             current_game_dict['games'][str(self.game_id)].update(players)
-            print current_game_dict
+            self.game_data = current_game_dict
         else:
             self.game_id = max_game_id + 1
-            max_game_id = self.game_id
-            pass
-        pass
+            current_game_dict['games'][str(self.game_id)] = players
+            self.game_data = current_game_dict
+            
+    def AddScore(self):
+        orig_dict = self.game_data
+        temp_dict = orig_dict['games'][str(self.game_id)]['players']
+        nbr_players = len(temp_dict)
+        for i in range(1, nbr_players + 1):
+            player = "p" + str(i)
+            vp = self.getVPIncome(player)
+            cards = self.getCardIncome(player)
+            temp_dict = self.update_player(temp_dict, player, self.round, rnd_vp=vp, rnd_cards=cards)
+        self.round += 1
+        print temp_dict
+            
     
     def getWidgets(self, name, cls=False):
         widgets = []
@@ -121,7 +134,7 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
                 widgets.append(widget)
         return widgets
 
-    def getPlayers(self):
+    def loadPlayers(self):
         widgets = (self.player_name_grid.itemAt(i).widget() for i in range(self.player_name_grid.count()))
         return_dict = dict.fromkeys(["players"], {})
         for w in widgets:
@@ -129,13 +142,42 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
                 if str(w.currentText()) == "":
                     pass
                 else:
-                    return_dict["players"].update({str(w.objectName())[-2:]: {"player_name": str(w.currentText())}})
+                    return_dict["players"].update({str(w.objectName())[-2:]: {"player_name": str(w.currentText()), "rounds": None}})
         return return_dict
         
     def loadGameData(self):
         with open(gameFile) as json_data:
             games = json.load(json_data)
         return games
+        
+    def getVPIncome(self, player):
+        widgets = (self.player_vp_grid.itemAt(i).widget() for i in range(self.player_vp_grid.count()))
+        for w in widgets:
+            if isinstance(w, QtGui.QSpinBox):
+                if str(w.objectName()[-2:]) == player:
+                    return w.value()
+                    
+    def getCardIncome(self, player):
+        widgets = (self.player_card_grid.itemAt(i).widget() for i in range(self.player_card_grid.count()))
+        for w in widgets:
+            if isinstance(w, QtGui.QSpinBox):
+                if str(w.objectName()[-2:]) == player:
+                    return w.value()
+                
+    def getPlayerTotalVP(self, data, rnd_vp=0):
+        total = 0
+        for k, v in data.items():
+            total += v['round_vp_total']
+        total += rnd_vp
+        return total
+        
+    def update_player(self, data, player, rnd=1, rnd_vp=0, rnd_cards=0):
+        if rnd == 1:
+            data[player].update({'rounds': {str(rnd): {'card_income': rnd_cards, 'round_vp': rnd_vp, 'round_vp_total': rnd_vp}}})
+        else:
+            total_vp = self.getPlayerTotalVP(data[player]['rounds'], rnd_vp)
+            data[player]['rounds'][str(rnd)] = {'card_income': rnd_cards, 'round_vp': rnd_vp, 'round_vp_total': total_vp}
+        return data
             
 
 
